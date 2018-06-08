f75e96609e7b768a6cfd8b7d5acee36e98ca9b0e
Dominic Lee <34794189+dominictwlee@users.noreply.github.com>
2018-06-08 13:29:14 +0100

    Prevent mongodb from creating collection if collection already exists

--- config/db.js
+++ config/db.js
@@ -1,28 +1,34 @@
 const { MongoClient } = require('mongodb');
 
 const url = 'mongodb://localhost:27017';
-const dbName = 'billtracker';
+const dbName = process.env.DB_NAME;
 
 module.exports = () =>
   MongoClient.connect(url).then(client => {
     const db = client.db(dbName);
-    db.createCollection('bills', {
-      validator: {
-        $jsonSchema: {
-          bsonType: 'object',
-          required: ['bill', 'cost'],
-          properties: {
-            bill: {
-              type: 'string',
-              description: 'Must be a string and is required',
+    db.listCollections({ name: 'bills' })
+      .toArray()
+      .then(list => {
+        if (list.length === 0) {
+          db.createCollection('bills', {
+            validator: {
+              $jsonSchema: {
+                bsonType: 'object',
+                required: ['bill', 'cost'],
+                properties: {
+                  bill: {
+                    type: 'string',
+                    description: 'Must be a string and is required',
+                  },
+                  cost: {
+                    type: 'number',
+                    description: 'Must be a number and is required',
+                  },
+                },
+              },
             },
-            cost: {
-              type: 'number',
-              description: 'Must be a number and is required',
-            },
-          },
-        },
-      },
-    });
+          });
+        }
+      });
     return db;
   });
--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const request = require('supertest'); // eslint-disable-line
 const app = require('../server');
 
 describe('GET /', () => {
-  test('should return hello world', done => {
+  it('should return hello world', done => {
     request(app)
       .get('/')
       .expect(200)
@@ -11,9 +11,10 @@ describe('GET /', () => {
 });
 
 describe('POST /api/bills', () => {
-  test('should return POST result', done => {
+  it('should return POST result', done => {
     request(app)
       .post('/api/bills')
+      .send({ bill: 'Electricity', cost: 50 })
       .expect(200)
       .end(done);
   });
